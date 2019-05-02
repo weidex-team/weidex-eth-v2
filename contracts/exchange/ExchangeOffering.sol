de47870b7a474af275f7c48e0497b391f3207d43
Krasimir Raykov <kraykov1994@gmail.com>
2019-05-02 16:28:56 +0300

    Add exchange offering

---
+++ contracts/exchange/ExchangeOffering.sol
@@ -0,0 +1,122 @@
+pragma solidity >=0.4.22 <0.6.0;
+pragma experimental ABIEncoderV2;
+
+import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+import "../utils/LibCrowdsale.sol";
+import "./ExchangeStorage.sol";
+
+contract ExchangeOffering is ExchangeStorage, LibCrowdsale {
+
+    address constant internal ETH_ADDRESS = address(0);
+
+    using SafeERC20 for IERC20;
+
+    using SafeMath for uint256;
+
+    mapping(address => Crowdsale) public crowdsales;
+
+    mapping(address => mapping(address => uint256)) public contributions;
+
+    event TokenPurchase(
+        address indexed project,
+        address indexed user,
+        uint256 tokens,
+        uint256 weiAmount
+    );
+
+    function registerCrowdsale(
+        Crowdsale memory crowdsale,
+        address token
+    )
+        public
+        onlyOwner
+    {
+        require(
+            CrowdsaleStatus.VALID == isValid(crowdsale),
+            "INVALID_CROWDSALE"
+        );
+
+        IERC20(token).safeTransferFrom(crowdsale.wallet, address(this), crowdsale.hardCap);
+
+        crowdsales[token] = crowdsale;
+    }
+
+    function buyTokens(address token)
+       public
+       payable
+    {
+        require(msg.value != 0, "INVALID_MSG_VALUE");
+
+        uint256 weiAmount = msg.value;
+
+        address user = msg.sender;
+
+        Crowdsale memory crowdsale = crowdsales[token];
+
+        require(
+            ContributionStatus.VALID == validContribution(weiAmount, crowdsale, user, token),
+            "INVALID_CONTRIBUTION"
+        );
+
+        uint256 purchasedTokens = weiAmount.mul(crowdsale.tokenRatio);
+
+        crowdsale.leftAmount = crowdsale.leftAmount.sub(purchasedTokens);
+
+        crowdsale.weiRaised = crowdsale.weiRaised.add(weiAmount);
+
+        balances[ETH_ADDRESS][crowdsale.wallet] = balances[ETH_ADDRESS][crowdsale.wallet].add(weiAmount);
+
+        balances[token][user] = balances[token][user].add(purchasedTokens);
+
+        contributions[token][user] = contributions[token][user].add(weiAmount);
+
+        crowdsales[token] = crowdsale;
+
+        emit TokenPurchase(token, user, purchasedTokens, weiAmount);
+    }
+
+    function burnTokensWhenFinished(address token) public 
+    {
+        require(
+            isFinished(crowdsales[token].endBlock),
+            "CROWDSALE_NOT_FINISHED_YET"
+        );
+
+        uint256 leftAmount = crowdsales[token].leftAmount;
+
+        crowdsales[token].leftAmount = 0;
+
+        IERC20(token).safeTransfer(ETH_ADDRESS, leftAmount);
+    }
+
+    function validContribution(
+        uint256 weiAmount,
+        Crowdsale memory crowdsale,
+        address user,
+        address token
+    )
+        public
+        view
+        returns(ContributionStatus)
+    {
+        if (!isOpened(crowdsale.startBlock, crowdsale.endBlock)) {
+            return ContributionStatus.CROWDSALE_NOT_OPEN;
+        }
+
+        if(weiAmount < crowdsale.minContribution) {
+            return ContributionStatus.MIN_CONTRIBUTION;
+        }
+
+        if (contributions[token][user].add(weiAmount) > crowdsale.maxContribution) {
+            return ContributionStatus.MAX_CONTRIBUTION;
+        }
+
+        if (crowdsale.hardCap < crowdsale.weiRaised.add(weiAmount)) {
+            return ContributionStatus.HARDCAP_REACHED;
+        }
+
+        return ContributionStatus.VALID;
+    }
+}
---
+++ contracts/utils/LibCrowdsale.sol
@@ -0,0 +1,75 @@
+pragma solidity >=0.4.22 <0.6.0;
+pragma experimental ABIEncoderV2;
+
+contract LibCrowdsale {
+
+    struct Crowdsale {
+        uint256 startBlock;
+        uint256 endBlock;
+        uint256 hardCap;
+        uint256 leftAmount;
+        uint256 tokenRatio;
+        uint256 minContribution;
+        uint256 maxContribution;
+        uint256 weiRaised;
+        address wallet;
+    }
+
+    enum ContributionStatus {
+        CROWDSALE_NOT_OPEN,
+        MIN_CONTRIBUTION,
+        MAX_CONTRIBUTION,
+        HARDCAP_REACHED,
+        VALID
+    }
+
+    enum CrowdsaleStatus {
+        INVALID_START_BLOCK,
+        INVALID_END_BLOCK,
+        INVALID_TOKEN_RATIO,
+        INVALID_LEFT_AMOUNT,
+        VALID
+    }
+
+    function isValid(Crowdsale memory crowdsale)
+        internal
+        view
+        returns (CrowdsaleStatus)
+    {
+
+        if(crowdsale.startBlock < block.number) {
+            return CrowdsaleStatus.INVALID_START_BLOCK;
+        }
+
+        if(crowdsale.endBlock < crowdsale.startBlock) {
+            return CrowdsaleStatus.INVALID_END_BLOCK;
+        }
+
+        if(crowdsale.tokenRatio == 0) {
+            return CrowdsaleStatus.INVALID_TOKEN_RATIO;
+        }
+
+        if(crowdsale.hardCap != crowdsale.leftAmount) {
+            return CrowdsaleStatus.INVALID_LEFT_AMOUNT;
+        }
+
+        return CrowdsaleStatus.VALID;
+    }
+
+    function isOpened(uint256 startBlock, uint256 endBlock)
+        internal
+        view
+        returns (bool)
+    {
+        return (block.number >= startBlock && block.number <= endBlock);
+    }
+
+
+    function isFinished(uint256 endBlock)
+        internal
+        view
+        returns (bool)
+    {
+        return block.number > endBlock;
+    }
+}
